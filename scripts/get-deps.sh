594bbe431861b92a474347d9bd382f6f6a09bc15
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-08-18 14:37:49 +0300

    add project files

---
+++ scripts/get-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd $(dirname "$0")/../..
+
+WORKSPACE_PATH=$(pwd)
+
+git clone git@github.com:cuttle-system/cuttle-parser.git
+cuttle-parser/scripts/get-deps.sh
+
+cd $WORKSPACE_PATH
+
+git clone git@github.com:cuttle-system/cuttle-translator.git
+cuttle-translator/scripts/get-deps.sh
+
+cd $WORKSPACE_PATH
+
+git clone git@github.com:cuttle-system/cuttle-test.git
