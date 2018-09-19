826b785e211c804c9701baa15fab5f44defa2c96
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-09-19 12:35:51 +0300

    update the project to compile under windows

--- tests/test_presenters_map.cpp
+++ tests/test_presenters_map.cpp
@@ -18,7 +18,7 @@ BOOST_FIXTURE_TEST_SUITE(presenters_map_add_suite, base_presenters_map_fixture)
             return std::string("function1");
         };
         auto func2 = [](int argi, bool is_func) {
-            return std::string("functoin2");
+            return std::string("function2");
         };
         auto func3 = [](int argi, bool is_func) {
             return false;
@@ -29,18 +29,23 @@ BOOST_FIXTURE_TEST_SUITE(presenters_map_add_suite, base_presenters_map_fixture)
         BOOST_CHECK_EQUAL(presenters_map["+"].skip, func3);
     }
 
+#ifndef _WIN32
+
     BOOST_AUTO_TEST_CASE(case2) {
         auto func1 = [](int argi, bool is_func) {
             return std::string("function1");
         };
-        auto func2 = nullptr;
+        generator_separator_func_t *func2 = nullptr;
         auto func3 = [](int argi, bool is_func) {
             return false;
         };
         add(presenters_map, "+", func1, func2, func3);
         BOOST_CHECK_EQUAL(presenters_map["+"].left_separator, func1);
-        BOOST_CHECK_EQUAL(presenters_map["+"].right_separator, generator::defaults::basic::presenter_right_separator);
+        generator_separator_func_t *func = presenters_map["+"].right_separator;
+        std::cout << (long long)func << " " << (long long)generator::defaults::basic::presenter_right_separator << std::endl;
+        BOOST_CHECK((long long)func == (long long)generator::defaults::basic::presenter_right_separator);
         BOOST_CHECK_EQUAL(presenters_map["+"].skip, func3);
+        BOOST_CHECK_EQUAL("", presenters_map["+"].right_separator(2, true));
     }
 
     BOOST_AUTO_TEST_CASE(case3) {
@@ -61,5 +66,7 @@ BOOST_FIXTURE_TEST_SUITE(presenters_map_add_suite, base_presenters_map_fixture)
         BOOST_CHECK_EQUAL(presenters_map["+"].right_separator, generator::defaults::basic::presenter_right_separator);
         BOOST_CHECK_EQUAL(presenters_map["+"].skip, generator::defaults::basic::presenter_skip);
     }
+
+#endif
 
 BOOST_AUTO_TEST_SUITE_END()
